3dc0438f8d0da0831e84607d29fe55808aa93429
elifelse <elifgsavukcam@gmail.com>
2021-06-29 12:31:32 +0300

    ef model

---
+++ SanatOkulu/Models/Eser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace SanatOkulu.Models
+{
+    public class Eser
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Ad { get; set; }
+        public int? Yil { get; set; }
+        // Sanatci + Id : Sanatci enetity'sinin Id'sine referans veren bir FK
+        public int SanatciId { get; set; }
+
+        // Bu eserin 1 sanatçısını vardır (1-çok ilişki)
+        public virtual Sanatci Sanatci { get; set; }
+    }
+}
---
+++ SanatOkulu/Models/Sanatci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanatOkulu.Models
+{
+    [Table("Sanatcilar")]
+    public class Sanatci
+    {
+        public int Id { get; set; }
+        
+        [Required, MaxLength(50)]
+        public string Ad { get; set; }
+
+        // virtual koyduğunuzda bu navigation property 
+        // LAZY LOADING aktif olur.
+        public virtual ICollection<Eser> Eserler { get; set; }
+
+    }
+}
